453aa1ad95c5bd4db68124a1cc42d48f7efe52c4
rluver <skyd0608@gmail.com>
2022-01-02 21:39:23 +0900

    feat: Add pastor_stambaugh_liquidity
    feat:
     - add pastor_stambaugh_liquidity to Liquidity class

--- Chapter 08：Time-Series Analysis/time_series.py
+++ Chapter 08：Time-Series Analysis/time_series.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pandas_datareader.data as web
 import numpy as np
 import scipy as sp
+import statsmodels.api as sm
 from scipy import stats
 
 
@@ -40,11 +41,59 @@ def roll_spread(data: any([pd.DataFrame, list]), col_names=None) -> float:
 
     return cov
 
-def amihud_illiquidity(data: pd.DataFrame) -> float:
-    closing_price = data['Adj Close'].to_numpy()
-    volume = data.Volume.to_numpy() * closing_price
-    returns = np.diff(closing_price) / closing_price[1:]
 
-    illiquidity = np.divide(np.abs(returns), volume[1:]).mean()
+class Liquidity:
 
-    return illiquidity
+    @staticmethod
+    def amihud_illiquidity(data: pd.DataFrame) -> float:
+        closing_price = data['Adj Close'].to_numpy()
+        volume = data.Volume.to_numpy() * closing_price
+        returns = np.diff(closing_price) / closing_price[1:]
+
+        illiquidity = np.divide(np.abs(returns), volume[1:]).mean()
+
+        return illiquidity
+
+    @staticmethod
+    def pastor_stambaugh_liquidity(data: pd.DataFrame, ff):
+        '''
+        y_t = alpha + beta_1 * x_(1, t-1) = beta_2 * x_(2, t_2) + epsilon
+
+        y_t  = R_t - R_f (excess stock return on day t)
+               R_t: the return for the stock
+               R_f: the risk-free rate
+
+        x_(1, t) is the market return
+        x_(2, t) is the signed dollar trading volume
+
+        x_(2, t) = sign(R_t - R_(f, t) * P_t * volume)
+                   P_t: the stock price
+                   
+        beta2: Pastor and Stambaugh's liquidity'
+        '''
+        returns = np.array([x/y - 1 for x, y in zip(MSFT['Adj Close'][1:], MSFT['Adj Close'][:-1])])
+        dollar_volume = np.array([x * y for x, y in zip(MSFT['Adj Close'][1:], MSFT['Volume'])])
+        dates = data.index
+
+        tt = pd.DataFrame(returns, index=dates[1:], columns=['returns'])
+        tt2 = pd.DataFrame(dollar_volume, index=dates[1:], columns=['dollar_volume'])
+
+        ff_dates = ff.DATE.copy()
+        ff = pd.DataFrame(ff.iloc[:, 1:])
+        ff.index = ff_dates
+
+        tt3 = pd.merge(tt, tt2, left_index=True, right_index=True)
+        final = pd.merge(tt3, ff, left_index=True, right_index=True)
+
+        y = [x - y for x, y in zip(final.returns[1:], final.RF[1:])]
+        x1 = final.MKT_RF[:-1]
+        x2 = np.sign(np.array(final.returns[:-1] - final.RF[:-1])) * np.array(final.dollar_volume[:-1])
+        x3 = [x1, x2]
+
+        n = np.size(x3)
+        x = np.reshape(x3, [int(n/2), 2])
+        x = sm.add_constant(x)
+
+        results = sm.OLS(y, x).fit()
+
+        return results
