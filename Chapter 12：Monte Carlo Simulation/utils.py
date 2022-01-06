bf39ff0d5df9d7ff53c88c82cea6a52fd5d3bbd6
rluver <skyd0608@gmail.com>
2022-01-06 15:34:22 +0900

    feat: Edit utils.py to include StockPriceMovement class
    feat:
     - add StockPriceMovement class

--- Chapter 12：Monte Carlo Simulation/utils.py
+++ Chapter 12：Monte Carlo Simulation/utils.py
@@ -77,3 +77,64 @@ class AnnualReturnsDistribution:
         plt.figtext(0.63, 0.8, f'mean annual={mean_annual}')
         plt.hist(self.estimated_returns, 50)
         plt.show()
+
+
+class MonteCarloSimulation:
+    pass
+
+
+class StockPriceMovement:
+    """
+
+    StockPriceMovement class is to simulate stock price movement.
+
+    Below is an explanation for arguments in this class.
+
+    stock_price_today: stock price at time zero
+    T: maturity date (in years)
+    n_steps: number of steps
+    mu: expected annual return
+    sigma: annualized volatility
+    n_simulation: number of simulations
+
+    """
+
+    def __init__(self, stock_price_today: float, T: float, n_steps: int,
+                 mu: float, sigma: float, n_simulations: int):
+        self.stock_price_today = stock_price_today
+        self.T = T
+        self.n_steps = n_steps
+        self.mu = mu
+        self.sigma = sigma
+        self.n_simulations = n_simulations
+        self.S = [[] for _ in range(n_simulations)]
+
+    def __call__(self):
+        self._get_simulated_stock_price()
+        self._show_simulated_stock_price()
+
+    def _get_simulated_stock_price(self):
+        dt = self.T / self.n_steps
+    
+        self.x = range(self.n_steps)
+
+        for n in range(self.n_simulations):
+            self.S[n] = np.zeros([self.n_steps])
+            self.S[n][0] = self.stock_price_today
+            for i in self.x[:-1]:
+                e = np.random.normal()
+                self.S[n][i + 1] = (self.S[n][i] +
+                                    self.S[n][i] * (self.mu -
+                                                    0.5 * (self.sigma ** 2)
+                                                    ) * dt +
+                                    self.sigma * self.S[n][i] * np.sqrt(dt) * e)
+
+    def _show_simulated_stock_price(self):
+        for i in range(self.n_simulations):
+            plt.plot(self.x, self.S[i])
+        plt.figtext(0.2, 0.8, f'S0={self.S[i][0]}, mu={self.mu}, sigma={self.sigma}')
+        plt.figtext(0.2, 0.76, f'T={self.T}, steps={self.n_steps}')
+        plt.title(f'Stock price (number of simulation = {self.n_simulations})')
+        plt.xlabel(f'Total number of steps = {self.n_steps}')
+        plt.ylabel('stock price')
+        plt.show()
